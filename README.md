d0d375bfb9be4c18a43dba987b5204197b7a0977
Prashant Maharishi <70382629+hypertext-assains-ietagra@users.noreply.github.com>
2022-04-07 15:40:47 +0530

    Add files via upload
    Work done ..

---
+++ README.md
@@ -0,0 +1,44 @@
+<h1 align="center">Grocery Application</h1>
+
+<p align="center">
+  <img 
+    src="https://user-images.githubusercontent.com/70382629/162154934-5fc3f92f-2960-4121-b1fb-595d981ca2ef.png"
+  >
+</p>
+
+
+## Introduction
+<p align="left">
+This is C++ based QT application. 
+  As the name suggests
+We
+</p>
+
+## Tools
+* Qt Creator
+* Qt Designer
+* sqlite3 database
+
+## Fratures
+
+* Add Item 
+
+    ![Screenshot from 2022-04-07 14-11-17](https://user-images.githubusercontent.com/70382629/162158928-297e66a2-3d0c-41d1-865c-f99f13d41ae7.png)
+
+* Update Item
+
+![Screenshot from 2022-04-07 14-14-48](https://user-images.githubusercontent.com/70382629/162159170-d280fde6-1a13-499d-96b1-3d0ceb9dd5b7.png)
+
+* Find Item
+
+![Screenshot from 2022-04-07 14-15-41](https://user-images.githubusercontent.com/70382629/162159481-b0b25d07-c484-41fa-bbc6-8616199b4424.png)
+
+* Expired Item
+
+
+* Available Item
+* Spoiled Item
+* Return Item
+* Report to Owner
+
+
---
+++ src/additem.cpp
@@ -0,0 +1,72 @@
+#include "additem.h"
+#include "ui_additem.h"
+
+AddItem::AddItem(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::AddItem)
+{
+    ui->setupUi(this);
+}
+
+AddItem::~AddItem()
+{
+    delete ui;
+}
+
+void AddItem::Show()
+{
+    this->show();
+}
+
+void AddItem::on_btnAdd_clicked()
+{
+    ui->lblInfo->setText("");
+    QString sName = ui->txtName->text();
+    QString sQuantity = ui->txtQuantity->text();
+    QString sStatus = ui->cmbStatus->currentText();
+    QString sDOP = ui->txtDOP->text();
+    QString sDOS = ui->txtDOS->text();
+    QString sSellingPrice = ui->txtSellingPrice->text();
+    QString sPurchasePrice = ui->txtPurchasePrice->text();
+
+    QSqlQuery query(MyDB::getInstance()->getDBInstance());
+    query.clear();
+
+    query.prepare("insert into groceryitems(Name, Quantity, Status, DOP, DOS, SellingPrice, PurchasePrice) values (:Name, :Quantity, :Status, :DOP, :DOS, :SellingPrice, :PurchasePrice)");
+    query.bindValue(":Name", sName);
+    query.bindValue(":Quantity", sQuantity);
+    query.bindValue(":Status", sStatus);
+    query.bindValue(":DOP", sDOP);
+    query.bindValue(":DOS", sDOS);
+    query.bindValue(":SellingPrice", sSellingPrice);
+    query.bindValue(":PurchasePrice", sPurchasePrice);
+
+
+    if(!query.exec())
+    {
+        qDebug() << query.lastError().text() << query.lastQuery();
+        ui->lblInfo->setText("Unable to Add Item");
+    }
+    else
+    {
+        if(query.numRowsAffected() > 0)
+        {
+            qDebug() <<"read was successful "<< query.lastQuery();
+            ui->lblInfo->setText("Item Added Successfully!");
+            resetElements();
+        }
+        else
+            qDebug() <<"Unable to Add new Item";
+     }
+
+}
+
+void AddItem::resetElements()
+{
+    ui->txtName->clear();
+    ui->txtQuantity->clear();
+    ui->txtDOP->clear();
+    ui->txtDOS->clear();
+    ui->txtSellingPrice->clear();
+    ui->txtPurchasePrice->clear();
+}
